0303db1911655290b35ba3b30cf9d3e40a8b966b
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-20 19:12:40 +0000

    Recipe Finish

--- recipe.c
+++ recipe.c
@@ -7,5 +7,14 @@ int main(void)
     printf("\n");
     printf("1 1/2 cups \t graham cracker crumbs (about 24 full-sized graham crackers)\n");
     printf("1/2 cup \t butter or margarine, melted\n");
-    printf("1 can (12 ounces/340 g) \t sweetened condensed milk\n");
+    printf("1 can \t \t sweetened condensed milk\n");
+    printf("1 cup \t \t flaked coconut\n");
+    printf("1 cup \t \t flaked coconut\n");
+    printf("1 cup \t \t dried cranberries\n");
+    printf("2 cups \t \t vanilla chips\n");
+    printf("1 cup \t \t sliced almonds or pecans\n");
+    printf("\n");
+    printf("1. \t Combine graham cracker crumbs and butter. \n Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n");
+    printf("2. \t Combine remaining ingredients and spread over graham cracker mixture. \n Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. \n Let cool and cut into squares. \n");
+    printf("Serve and Enjoy! \n");
 }
