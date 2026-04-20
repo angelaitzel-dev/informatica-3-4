e0b81668a23d03ac2133ae1a8f128c969ab8802f
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-20 14:53:14 +0000

    Recipe progress

---
+++ recipe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    printf("Coconut Cranberry Dessert\n");
+    printf("\n");
+    printf("1 1/2 cups \t graham cracker crumbs (about 24 full-sized graham crackers)\n");
+    printf("1/2 cup \t butter or margarine, melted\n");
+    printf("1 can (12 ounces/340 g) \t sweetened condensed milk\n");
+}
