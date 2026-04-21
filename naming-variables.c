957563160485cfcf6433b96034fafbff979fc82a
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-21 14:45:49 +0000

    Naming-Variables finished product

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int number; //took off the exclamation point
+int test_value; //change the space to an underscore
+int example1; //changed the 1 to the end since the first character has to be a letter
+float int_float; //i added "_float" to the int to make it run smoothly
+printf("Variables created successfully.\n");
+}
