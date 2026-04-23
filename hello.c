7bf0e1630196fb9eb54b91d752c60011b4f3f421
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-23 21:09:07 +0000

    Working on the input

--- hello.c
+++ hello.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
-    printf("Hello Angela! \n");
+    char name[50];
+    printf("Enter your name:");
+    scanf("%s", &name);
+    printf("Hello %s!\n", name);
+    int age[50];
+    printf("Enter your age:");
+    scanf("%d", &age);
+    printf("You are %d years old!\n", age);
+
 }
