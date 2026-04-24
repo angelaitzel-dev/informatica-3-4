7c11851e2028300a0ae50eb4de04e3cea0475193
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-24 00:02:40 +0000

    changes for the next class

--- hello.c
+++ hello.c
@@ -6,9 +6,13 @@ int main(void)
     printf("Enter your name:");
     scanf("%s", &name);
     printf("Hello %s!\n", name);
-    int age[50];
+    int age;
     printf("Enter your age:");
     scanf("%d", &age);
     printf("You are %d years old!\n", age);
+    int color[50];
+    printf("Enter your favorite color:");
+    scanf("%s", &color);
+    printf("Your favorite color is %s!\n", color);
 
 }
