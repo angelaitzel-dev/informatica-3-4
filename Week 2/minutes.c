42d639ba99affda8b768f7c818923ab8362a51a0
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-04-30 20:55:11 +0000

    I have no more braincells

--- Week 2/minutes.c
+++ Week 2/minutes.c
@@ -3,16 +3,17 @@
 int main(void)
 {
     int Minutes;
-    int Starttime;
+    int Starttimeh;
+    int Starttimem;
     printf("Enter the run time of the movie in minutes:");
     scanf("%d", &Minutes);
     printf("Enter the time you start the movie:");
-    scanf("%d", &Starttime);
+    scanf("%d:%d", &Starttimeh, &Starttimem);
     printf("\n");
     int Runtime= Minutes/60;
     int Runtime2= Minutes%60;
-    int Endtime= Starttime+Runtime;
-    int Endtime2= Starttime+Runtime2;
+    int Endtime= Starttimeh+Runtime;
+    int Endtime2= Starttimem+Runtime2;
     printf("The run time of the movie is %d hours and %d minutes\n", Runtime, Runtime2);
     printf("You will finish the movie at %d:%d \n", Endtime, Endtime2);
 }
