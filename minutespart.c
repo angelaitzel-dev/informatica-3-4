d24e9d721d1346cf7cb0bbb493b047c376115d69
angelaitzel-dev <kissy.angelaitzel@gmail.com>
2026-05-07 14:56:20 +0000

    Minutes doesnt work yet

---
+++ minutespart.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Minutes;
+    int Starttimeh;
+    int Starttimem;
+    printf("Enter the run time of the movie in minutes:");
+    scanf("%d", &Minutes);
+    printf("Enter the time you start the movie:");
+    scanf("%d:%d", &Starttimeh, &Starttimem);
+    printf("\n");
+
+    int Runtime= Minutes/60;
+    int Runtime2= Minutes%60;
+
+    int Endtime= Starttimeh+Runtime; //endtime = 8
+    int Endtime2= Starttimem+Runtime2; //endtime = 61
+
+    if (Endtime2 >= 60){
+        Endtime = Endtime + 1; //update variable value
+        Endtime2 = Endtime2 % 60;
+    }
+    
+    printf("The run time of the movie is %d hours and %d minutes\n", Runtime, Runtime2);
+    printf("You will finish the movie at %d:%02d \n", Endtime, Endtime2);
+}
